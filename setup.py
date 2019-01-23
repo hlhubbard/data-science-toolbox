5e4dac742c0440c1d2c5474680b1094c674dc11c
Hayley Hubbard <hubbard.hlb@gmail.com>
2019-01-23 23:06:52 +0000

    added numpy requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
-setup(name='hh_toolbox',
-      version='1.0',
-      description='Hayleys data science toolbox for mango solution',
-      author='Hayley Hubbard',
-      author_email='hubbard.hlb@gmail.com',
-      packages = [ 'hh_toolbox' ]
-     )
+with open('requirements.txt') as rf:
+  setup(
+    name='hh_toolbox',
+    version='1.0',
+    description='Hayleys data science toolbox for mango solution',
+    author='Hayley Hubbard',
+    author_email='hubbard.hlb@gmail.com',
+    packages = [ 'hh_toolbox' ],
+    install_requires=rf.readlines()
+  )
