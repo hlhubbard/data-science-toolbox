1b2b4fb826890b1967eebc80bfcc6b4ed27ffdd7
Hayley Hubbard <hubbard.hlb@gmail.com>
2019-01-23 21:21:25 +0000

    uploading toolbox to repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='hh_toolbox',
+      version='1.0',
+      description='Hayleys data science toolbox for mango solution',
+      author='Hayley Hubbard',
+      author_email='hubbard.hlb@gmail.com',
+      packages = [ 'hh_toolbox' ]
+     )
